c35a40ffe90f33761cc715d312a432333d86407c
Äkwav <ekwav@coflnet.com>
2025-10-19 00:14:36 +0200

    bump socket version to 1.7.7

--- src/main/java/CoflCore/CoflCore.java
+++ src/main/java/CoflCore/CoflCore.java
@@ -22,7 +22,7 @@ import org.greenrobot.eventbus.EventBus;
 
 public class CoflCore {
     public static final String MODID = "CoflSky";
-    public static final String VERSION = "1.7.6";
+    public static final String VERSION = "1.7.7";
 
     public static FlipHandler flipHandler = new FlipHandler();
 
