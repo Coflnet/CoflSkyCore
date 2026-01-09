a963634dd8ccce5441a4dc535aa3ad9b49e994c9
Äkwav <ekwav@coflnet.com>
2026-01-09 13:48:40 +0100

    bump socket version

--- src/main/java/CoflCore/CoflCore.java
+++ src/main/java/CoflCore/CoflCore.java
@@ -22,7 +22,7 @@ import org.greenrobot.eventbus.EventBus;
 
 public class CoflCore {
     public static final String MODID = "CoflSky";
-    public static final String VERSION = "1.8.1";
+    public static final String VERSION = "1.8.3";
 
     public static FlipHandler flipHandler = new FlipHandler();
 
--- src/main/java/CoflCore/network/NetworkUtils.java
+++ src/main/java/CoflCore/network/NetworkUtils.java
@@ -122,7 +122,7 @@ public class NetworkUtils {
             }
         }
         
-        connection.setRequestProperty("User-Agent", "SkyCoflMod/1.8.1");
+        connection.setRequestProperty("User-Agent", "SkyCoflMod/1.8.3");
         connection.setConnectTimeout(10000);
         connection.setReadTimeout(10000);
         
