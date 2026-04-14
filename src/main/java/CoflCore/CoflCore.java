c41172783ccf334a9144c21ea785ba51ba4834b8
Äkwav <ekwav@coflnet.com>
2026-04-14 17:44:56 +0200

    update socket to version 1.8.8

--- src/main/java/CoflCore/CoflCore.java
+++ src/main/java/CoflCore/CoflCore.java
@@ -22,7 +22,7 @@ import org.greenrobot.eventbus.EventBus;
 
 public class CoflCore {
     public static final String MODID = "CoflSky";
-    public static final String VERSION = "1.8.7";
+    public static final String VERSION = "1.8.8";
 
     public static FlipHandler flipHandler = new FlipHandler();
 
