dee462006d52e1783325a8af85caf9db51ea4b6a
Ekwav <ekwav@users.noreply.github.com>
2026-06-05 00:04:08 +0200

    downgrade to java 21

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,12 +36,12 @@ dependencies {
 
 java {
     withSourcesJar()
-    sourceCompatibility = JavaVersion.VERSION_25
-    targetCompatibility = JavaVersion.VERSION_25
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
 }
 
 tasks.withType<JavaCompile>().configureEach {
-    options.release.set(25)
+    options.release.set(21)
 }
 
 tasks.jar {
