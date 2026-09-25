2b997ee8e8976bbbd970c5db4f59fee40db34769
Äkwav <ekwav@coflnet.com>
2026-09-25 17:10:45 +0200

    Release socket protocol version 2.0.0

--- src/main/java/CoflCore/CoflCore.java
+++ src/main/java/CoflCore/CoflCore.java
@@ -24,7 +24,7 @@ import CoflCore.proxy.APIKeyManager;
 
 public class CoflCore {
     public static final String MODID = "CoflSky";
-    public static final String VERSION = "2.0.0-pre1";
+    public static final String VERSION = "2.0.0";
 
     public static FlipHandler flipHandler = new FlipHandler();
 
